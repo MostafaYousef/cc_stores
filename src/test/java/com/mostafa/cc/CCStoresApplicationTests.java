5e52e601531a539465e4aa98c66b6b38808590fd
MostafaYousef <mostafa.nonat@gmail.com>
2016-02-10 10:54:36 +0000

    updated test

--- src/test/java/com/mostafa/cc/CCStoresApplicationTests.java
+++ src/test/java/com/mostafa/cc/CCStoresApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CCStoresApplication.class)
 @WebAppConfiguration
@@ -13,6 +15,6 @@ public class CCStoresApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue(true);
 	}
-
 }
